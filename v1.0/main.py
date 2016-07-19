290eaa7b861c85423146e983e1e147fd5179252d
Mila432 <v-erfurt@t-online.de>
2016-07-20 00:18:03 +0200

    preparing for big update

--- v1.0/main.py
+++ v1.0/main.py
@@ -1,36 +1,45 @@
 import sys
 # Add the ptdraft folder path to the sys.path list
 sys.path.append('pgoapi/pgoapi')
+sys.path.append('pgoapi')
 from pgoapi import PGoApi
+import pokecli
 from utilities import f2i, h2f
+import logging
 
 from google.protobuf.internal import encoder
 from geopy.geocoders import GoogleV3
 from s2sphere import CellId, LatLng
 
-import time
-import re
-import random
-from datetime import datetime
-from getpass import getpass
-import argparse
 import os
 import platform
 	
 def main():
+	logging.basicConfig(level=logging.DEBUG, format='%(asctime)s [%(module)10s] [%(levelname)5s] %(message)s')
+	logging.getLogger("requests").setLevel(logging.WARNING)
+	logging.getLogger("pgoapi").setLevel(logging.INFO)
+	logging.getLogger("rpc_api").setLevel(logging.INFO)
 	if platform.system() == 'Windows':
 		os.system("title Pokemon GO API Python")
 		os.system("cls")
 	else:
 		# Catches "Lunux" and "Darwin" (OSX), among others
 		os.system("clear")
-	parser = argparse.ArgumentParser()
-	parser.add_argument("-u", "--username", help="Login", required=True)
-	parser.add_argument("-p", "--password", help="Password", required=True)
-	parser.add_argument("-t", "--type", help="Google/PTC", required=True)
-	parser.add_argument("-l", "--location", help="Location", required=True)
-	args = parser.parse_args()
-	print '[!] Using "%s" as login for %s..'%(args.type,args.username[:4],)
+	
+	config = pokecli.init_config()
+	if not config:
+		return
+		
+	position = pokecli.get_pos_by_name(config.location)
+	if config.test:
+		return
+		
+	api = PGoApi()
+	api.set_position(*position)
+	if not api.login(config.auth_service, config.username, config.password):
+		return
+	print '[!] todo from here ..'
+	exit()
 	
 if __name__ == '__main__':
 	main()
